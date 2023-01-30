0a3c0bd12e566be2113a930adfbaada85d0f3ae1
joengyamba <joycebaraka97@gmail.com>
2023-01-30 15:35:57 +0100

    feat/user

--- model/modelUsers.js
+++ model/modelUsers.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const userSchema=mongoose.Schema({
     name:{type:String, required:true   },
     mail:{type:String, required:true},
-    profilLink:{type:String, required:false},
+    profilLink:{type:String, required:true},
     facebookLink:{type:String, required:true},
     linkedinLink:{type:String,required:true}
 })
